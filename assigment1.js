822d3b504abc49640b3cbd23ade74427a1162383
Ricardo199 <ricardo1529@gmail.com>
2024-05-21 19:41:31 -0400

    comments

--- assigment1.js
+++ assigment1.js
@@ -1,4 +1,8 @@
+/* this file receives two integer variables and returns a grid */
+
+//grid function
 function Grid(a, b) {
+    //checking if the variables aren't null
     if (isNaN(b)) {
         b = 1;
     }
@@ -6,9 +10,11 @@ function Grid(a, b) {
         a = 1;
     }
 
+    //making the variables more understandable
     let row = a;
     let column = b;
     let table = "";
+    //nested for to make the table at different paces
     for (let i = 1; i <= row; i++) {
         table += "<tr>";
         for (let j = 1; j <= column; j++) {
@@ -16,33 +22,46 @@ function Grid(a, b) {
         }
         table += "</tr>";
     }
+    //making the table in the main html element
     document.getElementById("table").innerHTML = table;
 }
 
+//to avoid error this js file is loaded after the main html file is rendered
 window.onload = function () {
 
+    //assigning default values of the variables
     var a = 15;
     var b = 15;
 
+    //passing the variables with default values to the Grid method
     Grid(a, b);
-
+    
+    //the submit button
     document.getElementById('submit').onclick = function () {
 
+        //getting the value of the rows and columns given by the user
         var text = document.getElementById("txtQuantity").value;
 
+        //splitting the values by a comma to get two numbers, rows and columns
         const array = text.split(",");
 
+        //making sure the input is a number, place to introduce try catch block
         a = parseInt(array[0]);
         b = parseInt(array[1]);
 
+        //passing the variables to the Grid method
         Grid(a, b);
 
     }
     
+    //the reset button
     document.getElementById('reset').onclick = function () {
+
+        //setting the variables to their default values 
         a = 15;
         b = 15;
 
+        //sending the values to be rendered as a grid
         Grid(a, b);
     }
 }
