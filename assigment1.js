1a8dba71e16601b9ff60a6f9d6b2720b764e52de
Ricardo199 <ricardo1529@gmail.com>
2024-05-28 19:06:06 -0400

    date update

--- assigment1.js
+++ assigment1.js
@@ -1,6 +1,6 @@
 /* this file receives two integer variables and returns a grid 
 Author: Ricardo Burgos
-Date: 21/05/2024 (date of last modification)
+Date: 28/05/2024 (date of last modification)
 */
 
 //grid function
